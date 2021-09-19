e9ca25cc1874a8145ad1d459f004b2c0b97cec2f
Sarinsa <mikkemusbruker@gmail.com>
2021-09-19 22:34:34 +0200

    Basic project setup and structure.

---
+++ src/main/java/com/toast/blockproperties/common/config/Ayyy.java
@@ -0,0 +1,6 @@
+package com.toast.blockproperties.common.config;
+
+public class Ayyy {
+
+    // Hello! I am a dummy class that lets this package be pushed to github!
+}
---
+++ src/main/java/com/toast/blockproperties/common/core/BlockProperties.java
@@ -0,0 +1,16 @@
+package com.toast.blockproperties.common.core;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(BlockProperties.MODID)
+public class BlockProperties {
+
+    public static final String MODID = "blockproperties";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public BlockProperties() {
+
+    }
+}
